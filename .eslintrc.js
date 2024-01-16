24b652e0d0e50c62d79517193037baf2c5510627
Walter Munoz <walter.munoz@gmail.com>
2024-01-17 09:35:45 +1100

    Adding eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,50 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-hooks'],
+  extends: [
+    '@react-native',
+    'eslint:recommended',
+    'airbnb-base',
+    'prettier',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:import/errors',
+  ],
+  rules: {
+    // ...
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'comma-dangle': 0,
+    indent: ['error', 2],
+    'no-tabs': 0,
+    'react/prefer-stateless-function': 0,
+    'react/forbid-prop-types': 0,
+    'react/prop-types': 0,
+    'react/require-default-props': 0,
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'react/jsx-indent': [2, 2],
+    'react/jsx-indent-props': [2, 2],
+    'react/jsx-props-no-spreading': 0,
+    'max-len': [
+      'error',
+      {
+        code: 300,
+      },
+    ],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
 };
