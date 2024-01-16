323aea1bda5660f9e76848d5cb071fe170b004bb
Walter Munoz <walter.munoz@gmail.com>
2024-01-17 09:03:13 +1100

    Adding new structure to the project

---
+++ src/navigation/index.tsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import {Screens} from '../constants';
+import {HomeScreen} from '../screens/Home';
+
+const templateScreen: {[key: string]: React.JSX.Element} = {
+  [Screens.HOME]: <HomeScreen />,
+};
+
+export const NavigationContext = React.createContext({
+  currentScreen: Screens.HOME,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  navigate: (screen: string) => {},
+  back: () => {},
+});
+
+export const ContextNavigation = () => {
+  const [currentScreen, setCurrentScreen] = useState<string>(Screens.HOME);
+
+  const navigate = (screen: string) => {
+    setCurrentScreen(screen);
+  };
+
+  return (
+    <NavigationContext.Provider
+      value={{
+        currentScreen,
+        navigate,
+        back: () => setCurrentScreen(Screens.HOME),
+      }}>
+      {templateScreen[currentScreen]}
+    </NavigationContext.Provider>
+  );
+};
---
+++ src/screens/Home.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react/react-in-jsx-scope */
+import {View, Text} from 'react-native';
+
+export const HomeScreen: React.FC = () => (
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: 'white',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+    <Text>TODO:Layout </Text>
+  </View>
+);
