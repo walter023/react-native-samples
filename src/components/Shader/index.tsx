ec07ff336a7c3f23fe5fad15b4931bd67fc8c06d
Walter Munoz <walter.munoz@gmail.com>
2024-01-22 12:41:59 +1100

    Adding animation using the iTime uniform

--- src/components/Shader/index.tsx
+++ src/components/Shader/index.tsx
@@ -1,8 +1,10 @@
-import { Skia, Canvas, Shader, Fill, vec } from '@shopify/react-native-skia';
+import { Skia, Canvas, Shader, Fill, vec, useClock } from '@shopify/react-native-skia';
 import { useWindowDimensions } from 'react-native';
+import { useDerivedValue } from 'react-native-reanimated';
 
 const source = Skia.RuntimeEffect.Make(`
-uniform vec2 iResolution;  
+uniform vec2 iResolution;
+uniform float  iTime;    
 
 vec4 main(vec2 pos) {
   vec2 uv = pos / iResolution;
@@ -10,7 +12,7 @@ vec4 main(vec2 pos) {
   uv.x *= iResolution.x / iResolution.y;
   
   float d = length(uv);
-  d = sin(d * 12) / 12;
+  d = sin(d * 12 + iTime) / 12;
   d = abs(d);
   d = smoothstep(0.00, 0.03, d);
   
@@ -20,13 +22,16 @@ vec4 main(vec2 pos) {
 export const TryppyShader = () => {
   const { height, width } = useWindowDimensions();
   const iResolution = vec(width, height);
-  
+  const clock = useClock();
+
+  const uniforms = useDerivedValue(() => ({ iResolution, iTime: clock.value }), [clock]);
+
   return (
     <Canvas style={{ flex: 1 }}>
-    <Fill>
-    <Shader source={source} uniforms={{ iResolution }} />
-    </Fill>
+      <Fill>
+        <Shader source={source} uniforms={uniforms} />
+      </Fill>
     </Canvas>
-    );
-  };
-  
+  );
+};
+
