ed2241eac8d04dc5142d5906fc9b8b4f66396722
Walter Munoz <walter.munoz@gmail.com>
2024-01-22 11:24:23 +1100

    Normalize canvas in order to work with the unit vector

--- src/components/Shader/index.tsx
+++ src/components/Shader/index.tsx
@@ -1,15 +1,23 @@
-import { Skia, Canvas, Shader, Fill } from '@shopify/react-native-skia';
+import { Skia, Canvas, Shader, Fill, vec } from '@shopify/react-native-skia';
+import { useWindowDimensions } from 'react-native';
 
 const source = Skia.RuntimeEffect.Make(`
+uniform vec2 iResolution;  
+
 vec4 main(vec2 pos) {
-  return vec4(0.0, 0.0, 0.0, 1);
+ vec2 uv = pos / iResolution;
+
+ return vec4(uv.x, uv.y, 0.0, 1);
 }`)!;
 
 export const TryppyShader = () => {
+  const { height, width } = useWindowDimensions();
+  const iResolution = vec(width, height);
+
   return (
-    <Canvas style={{ width: 256, height: 256 }}>
+    <Canvas style={{ flex: 1 }}>
       <Fill>
-        <Shader source={source} />
+        <Shader source={source} uniforms={{ iResolution }} />
       </Fill>
     </Canvas>
   );
