ae8e941d79e35c2bb6c08b5c8941a82836c4a246
Walter Munoz <walter.munoz@gmail.com>
2024-01-25 10:38:56 +1100

    Adding type.

--- src/components/Reflection/ReflectionSkia.tsx
+++ src/components/Reflection/ReflectionSkia.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Canvas, Path } from '@shopify/react-native-skia';
 
-const ReflectionSkia = () => (
+const ReflectionSkia: React.FC = () => (
   <Canvas>
     <Path
       path="M 128 0 L 168 80 L 256 93 L 192 155 L 207 244 L 128 202 L 49 244 L 64 155 L 0 93 L 88 80 L 128 0 Z"
