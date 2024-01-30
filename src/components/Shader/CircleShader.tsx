ddcf2e02803619e8ff5512246d17836ddc40a68e
Walter Munoz <walter.munoz@gmail.com>
2024-01-31 10:51:27 +1100

    Adding gls base logic and palette function.

--- src/components/Shader/CircleShader.tsx
+++ src/components/Shader/CircleShader.tsx
@@ -1,7 +1,31 @@
-import { Canvas, Fill, Shader, useClock, vec } from '@shopify/react-native-skia';
+import { Canvas, Fill, Shader, Skia, useClock, vec } from '@shopify/react-native-skia';
 import React from 'react';
 import { StyleSheet, useWindowDimensions } from 'react-native';
 import { useDerivedValue } from 'react-native-reanimated';
+
+const source = Skia.RuntimeEffect.Make(`
+uniform vec2 iResolution;
+uniform float  iTime;   
+
+// https://iquilezles.org/articles/palettes/
+vec3 palette( in float t ){
+    vec3 a =  vec3 (0.618, 0.658,0.500);
+    vec3 b =  vec3 (-0.082, 0.500, -0.452);
+    vec3 c =  vec3 (1.000, 1.000, 1.000);
+    vec3 d =  vec3 (0.000, 0.333, 0.667);
+    return a + b*cos( 6.28318*(c*t+d) );
+} 
+
+vec4 main( vec2 pos ) {
+  vec2 uv = pos / iResolution; 
+  uv = uv * 2 - 1; 
+  uv.x *= iResolution.x / iResolution.y; 
+  float time =  iTime * 0.001;
+  vec3 finalColor = vec3(0);
+
+ 
+  return vec4(finalColor, 1);
+}`)!;
 
 const CircleShader = () => {
   const { height, width } = useWindowDimensions();
@@ -12,7 +36,7 @@ const CircleShader = () => {
   return (
     <Canvas style={{ ...styles.container, height, width }}>
       <Fill>
-        <Shader uniforms={uniforms} />
+        <Shader source={source} uniforms={uniforms} />
       </Fill>
     </Canvas>
   );
