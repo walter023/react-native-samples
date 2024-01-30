1b61f8b94857df323a3d1f7185a3ae4314a4888c
Walter Munoz <walter.munoz@gmail.com>
2024-01-31 10:45:32 +1100

    Adding component file.

---
+++ src/components/Shader/CircleShader.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CircleShader = () => (
+  <View>
+    <Text>This is the CircleShader component</Text>
+  </View>
+);
+
+export default CircleShader;
