4bb1724c5d51b62d82f0d79afaa9b7bdb118ab43
Walter Munoz <walter.munoz@gmail.com>
2024-01-30 12:49:33 +1100

    Adding repetition.

--- src/components/Shader/index.tsx
+++ src/components/Shader/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Skia, Canvas, Shader, Fill, vec, useClock } from '@shopify/react-native-skia';
-import { useWindowDimensions } from 'react-native';
+import { StyleSheet, useWindowDimensions } from 'react-native';
 import { useDerivedValue } from 'react-native-reanimated';
 
 const source = Skia.RuntimeEffect.Make(`
@@ -17,7 +17,6 @@ vec3 palette( in float t )
     return a + b*cos( 6.28318*(c*t+d) );
 } 
 
-https://www.youtube.com/watch?v=PMltMdi1Wzg
 float sdSegment( in vec2 p, in vec2 a, in vec2 b )
 {
     vec2 pa = p-a, ba = b-a;
@@ -29,15 +28,22 @@ vec4 main( vec2 pos ) {
   vec2 uv = pos / iResolution;
   uv = uv * 2 - 1;
   uv.x *= iResolution.x / iResolution.y;
+  float time =  iTime * 0.002;
+  vec2 uv0 = vec2(uv); // ref to the center.
 
-  float d = sdSegment(uv, vec2(0, -0.2), vec2(0, 0.2));
+    uv *= 2;  
+    uv = fract( uv ); 
+    uv -= 0.5;
+    
+    float d = sdSegment(uv, vec2(uv.x, 0), vec2(0,uv.y));
 
-  vec3 col = palette(d+iTime * 0.002);
-  d = sin(d * 10 + iTime * 0.002) / 10;
-  d = abs(d);
+    vec3 col = palette(d + time);
+    d = sin(d * 10 + time ) / 10;
+    d = abs(d);
 
-  d = 0.009 / d; 
-  col *= d;
+    d = 0.009 / d; 
+    col *= d;
+
   return vec4(col, 1);
 
 }`)!;
@@ -49,12 +55,19 @@ const Rings = () => {
   const uniforms = useDerivedValue(() => ({ iResolution, iTime: clock.value }), [clock]);
 
   return (
-    <Canvas style={{ flex: 1, width, height, position: 'absolute' }}>
+    <Canvas style={{ ...styles.container, height, width }}>
       <Fill>
         <Shader source={source} uniforms={uniforms} />
       </Fill>
     </Canvas>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'absolute',
+  },
+});
 
 export default Rings;
