f4befc9ec0001613295aac31529732641feb0625
Walter Munoz <walter.munoz@gmail.com>
2024-01-30 12:24:57 +1100

    Changing the shape.

--- src/components/Shader/index.tsx
+++ src/components/Shader/index.tsx
@@ -17,21 +17,29 @@ vec3 palette( in float t )
     return a + b*cos( 6.28318*(c*t+d) );
 } 
 
+https://www.youtube.com/watch?v=PMltMdi1Wzg
+float sdSegment( in vec2 p, in vec2 a, in vec2 b )
+{
+    vec2 pa = p-a, ba = b-a;
+    float h = clamp( dot(pa,ba)/dot(ba,ba), 0.0, 1.0 );
+    return length( pa - ba*h );
+}
+
 vec4 main( vec2 pos ) {
   vec2 uv = pos / iResolution;
   uv = uv * 2 - 1;
   uv.x *= iResolution.x / iResolution.y;
-  
-  float d = length(uv);
-  vec3 col = palette(d);
+
+  float d = sdSegment(uv, vec2(0, -0.2), vec2(0, 0.2));
+
+  vec3 col = palette(d+iTime * 0.002);
   d = sin(d * 10 + iTime * 0.002) / 10;
   d = abs(d);
 
-  d = 0.015 / d; 
-
-
+  d = 0.009 / d; 
   col *= d;
   return vec4(col, 1);
+
 }`)!;
 
 const Rings = () => {
