ef63193b97e9403bf0e249a76779bb33e2ef9ec6
Walter Munoz <walter.munoz@gmail.com>
2024-02-06 08:35:37 +1100

    Adding consts.

--- src/helpers/helper.test.ts
+++ src/helpers/helper.test.ts
@@ -1,18 +1,26 @@
 import { lerp, intersectionPoint } from './index.ts';
 
 describe('lerp', () => {
+  const FROM = 0;
+  const TO = 1;
+
   test('The linear interpolation of 0 and 1 at 0.5 is 0.5', () => {
-    expect(lerp(0, 1, 0.5)).toBe(0.5);
+    const POS = 0.5;
+    const RESULT = 0.5;
+    expect(lerp(FROM, TO, POS)).toBe(RESULT);
   });
 
   test('The linear interpolation of 0 and 1 at 0.75 is 0.75', () => {
-    expect(lerp(0, 1, 0.75)).toBe(0.75);
+    const POS = 0.75;
+    const RESULT = 0.75;
+    expect(lerp(0, 1, POS)).toBe(RESULT);
   });
 });
 
 describe('intersectionPoint', () => {
   const ORIGIN = { x: 0, y: 0 };
   const RESOLUTION = { x: 100, y: 100 };
+
   test('The intersection point of a vector with origin (0, 0) and resolution (100, 100) is (100, 100)', () => {
     const INCOMING_VECTOR = { x: 100, y: 100 };
     const RESULT = { x: 100, y: 100 };
