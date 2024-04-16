d6784aba94ce66c852d07e81febae20ffa0672b4
Walter Munoz <walter.munoz@gmail.com>
2024-04-17 09:24:50 +1000

    Movig file.

--- src/screens/CircleShader.tsx
+++ src/screens/CircleShader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '../layout/index.tsx';
 import BackButton from '../components/BackButton/index.tsx';
-import CircleShader from '../components/Shader/CircleShader.tsx';
+import CircleShader from '../components/Fractal/index.tsx';
 
 const CircleShaderScreen: React.FC = () => (
   <Layout>
